0843e84fda41f88e493bac07c4dc31b4f0ab4e95
huglester <huglester@gmail.com>
2012-11-06 19:43:03 +0200

    Adding LT tld support

---
+++ src/Wisdom/Whois/Parser/Tld/Lt.php
@@ -0,0 +1,31 @@
+<?php
+
+/**
+ * This file is part of the Wisdom project.
+ *
+ *  (c) Саша Стаменковић <umpirsky@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Wisdom\Whois\Parser\Tld;
+
+use Wisdom\Whois\Parser\AbstractParser;
+
+/**
+ * Whois parser for .lt domains.
+ *
+ * @author Jaroslav Petrusevic (huglester@gmail.com)
+ */
+class Lt extends AbstractParser
+{
+
+	public function isAvailable()
+	{
+		// we strip multiple spaces here
+		$this->whois = preg_replace("/\s/", '', $this->whois);
+
+		return $this->contains('Status:available');
+	}
+}
